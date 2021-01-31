d7ebf63135032985329ca4369d9ddf27dc6eac0d
LEE KYO EUN <s2018s35@e-mirim.hs.kr>
2021-01-31 18:21:57 +0900

    Initial commit

---
+++ src/chap02/section1/defaultPackage.kt
@@ -0,0 +1,21 @@
+package chap02.section1
+
+import java.lang.Math.PI
+import java.lang.Math.abs
+
+//fun main(){
+//    val intro: String="안녕하세요!"
+//    val num=20;
+//
+//    println("intro: $intro, num: $num")
+//}
+
+fun main() {
+    val intro: String="안녕하세요!"
+    val num: Int=20
+
+    println(PI)
+    println(abs(-12.6))
+
+    println("intro: $intro, num: $num")
+}
